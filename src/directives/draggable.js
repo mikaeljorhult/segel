7c39fd186d3aa24bd7399e5cc2d3f88db2242e5a
Mikael Jorhult <mikael@jorhult.se>
2017-06-27 22:18:01 +0200

    Reevaluate draggable grid if window is resized.

--- src/directives/draggable.js
+++ src/directives/draggable.js
@@ -4,8 +4,27 @@
 import interact from 'interact.js';
 import Grid from '../helpers/grid';
 
+// Variables.
+let debounceTimer;
+
+const handleResize = function (element, vnode) {
+  clearTimeout(debounceTimer);
+
+  // Debounce grid calculations.
+  debounceTimer = setTimeout(function () {
+    // Set new grid based on current widths.
+    interact(element).draggable().snap.targets = Grid.create(
+      vnode.context.$root.$el.clientWidth,
+      35,
+      vnode.context.$root.steps
+    );
+  }, 150);
+};
+
 export default {
   bind: function (element, binding, vnode) {
+    window.addEventListener('resize', handleResize.bind(null, element, vnode));
+
     interact(element).draggable({
       snap: {
         targets: Grid.create(
@@ -41,7 +60,8 @@ export default {
       }
     });
   },
-  unbind: function (element) {
+  unbind: function (element, context, vnode) {
+    window.removeEventListener('resize', handleResize.bind(null, element, vnode));
     interact(element).unset();
   }
 };
