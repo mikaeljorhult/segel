a9a914ef1cfd69ae3e7b5135069359d4e3c76dbc
Mikael Jorhult <mikael@jorhult.se>
2017-07-03 19:28:47 +0200

    Validate changes before commiting them to stored state.

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Dependencies.
+import inRange from 'lodash/inRange';
+
 // Create state object.
 const Store = {
   state: {
@@ -23,16 +26,45 @@ const Store = {
     this.state.end = end;
   },
   addBooking: function (data) {
+    // Check availability off requested object.
+    if (!isAvailable(this.state.bookings, data)) { return; }
+
+    // Assign temporary ID to booking.
+    // TODO: Get the actual ID from user.
+    data.id = (Math.random() + 1).toString(36);
+
+    // Add booking to storage.
     this.state.bookings.push(data);
   },
   editBooking: function (data) {
+    // Check availability off requested object.
+    if (!isAvailable(this.state.bookings, data)) { return; }
+
+    // Retrieve the index of the stored copy of booking.
     let index = this.state.bookings.findIndex(function (element) {
       return element.id === data.id;
     });
 
+    // Replace booking with new data.
     this.state.bookings.splice(index, 1, data);
   }
 };
+
+function isAvailable (bookings, data) {
+  // Bookings for the requested object.
+  let objectBookings = bookings.filter(function (booking) {
+    // Same object as booking but ignore itself.
+    return booking.object === data.object &&
+      !(data.id !== undefined && booking.id === data.id);
+  });
+
+  // Return false if any bookings are in the same time slot.
+  return objectBookings.filter(function (booking) {
+    return inRange(data.start, booking.start - 1, booking.end) ||
+      inRange(data.end, booking.start + 1, booking.end) ||
+      inRange(booking.start, data.start, data.end);
+  }).length === 0;
+}
 
 // Return the instance.
 export default Store;
