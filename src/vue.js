ed1d6e90b83f0c603602f0390bc7e2a5f385e220
Mikael Jorhult <mikael@jorhult.se>
2017-08-07 20:10:12 +0200

    Don’t allow configuration through the HTML element.

--- src/vue.js
+++ src/vue.js
@@ -15,15 +15,6 @@ import Store from './store/store';
 const Instance = new Vue({
   template: '<segel-main v-bind:objects="objects" v-bind:bookings="bookings"></segel-main>',
 
-  beforeMount: function () {
-    // Get attributes from root element if present.
-    this.start = this.$el.getAttribute('start') ? parseInt(this.$el.getAttribute('start')) : this.start;
-    this.end = this.$el.getAttribute('end') ? parseInt(this.$el.getAttribute('end')) : this.end;
-    this.steps = this.$el.getAttribute('steps') ? parseInt(this.$el.getAttribute('steps')) : this.steps;
-    this.objects = this.$el.getAttribute('objects') ? this.$el.getAttribute('objects') : this.objects;
-    this.bookings = this.$el.getAttribute('bookings') ? this.$el.getAttribute('bookings') : this.bookings;
-  },
-
   components: {
     'segel-main': Main
   },
