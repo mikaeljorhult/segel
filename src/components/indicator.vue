d16185ed995e49524adc665d43533ed9f90b642a
Mikael Jorhult <mikael@jorhult.se>
2017-08-09 19:20:22 +0200

    Indicator to show current time.

---
+++ src/components/indicator.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        v-if="isInView"
+        v-bind:class="{'segel-indicator': true}"
+        v-bind:style="{left: left + '%'}"
+    ></div>
+</template>
+
+<script>
+  import inRange from 'lodash/inRange';
+
+  export default {
+    data: function () {
+      return {};
+    },
+
+    computed: {
+      isInView: function () {
+        return inRange(this.$store.state.currentTime, this.$store.state.start, this.$store.state.end);
+      },
+      left: function () {
+        return (this.$store.state.currentTime - this.$store.state.start) / this.$store.getters['duration'] * 100;
+      }
+    }
+  };
+</script>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,16 +1,20 @@
 <template>
     <section class="segel">
         <segel-navigation></segel-navigation>
-        <segel-ruler></segel-ruler>
-        <segel-objects
-                v-bind:objects="objects"
-                v-bind:bookings="bookings"
-        ></segel-objects>
+        <div class="segel-container">
+            <segel-indicator></segel-indicator>
+            <segel-ruler></segel-ruler>
+            <segel-objects
+                    v-bind:objects="objects"
+                    v-bind:bookings="bookings"
+            ></segel-objects>
+        </div>
     </section>
 </template>
 
 <script>
   import Navigation from './navigation.vue';
+  import Indicator from './indicator.vue';
   import Ruler from './ruler.vue';
   import Objects from './objects.vue';
 
@@ -26,6 +30,7 @@
 
     components: {
       'segel-navigation': Navigation,
+      'segel-indicator': Indicator,
       'segel-ruler': Ruler,
       'segel-objects': Objects
     }
