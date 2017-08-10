7b1f10d8aa28dbff3652a792f323ee8b2ff9b641
Mikael Jorhult <mikael@jorhult.se>
2017-08-10 17:20:49 +0200

    Setup Ava to support Vue single file components.

---
+++ test/api.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import Segel from '../src/index';
+
+test('that constructor is available', function (t) {
+  t.is(typeof Segel, 'function');
+});
--- test/test.js
+++ test/test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import test from 'ava';
 
 test('that tests are run correctly', function(t) {
