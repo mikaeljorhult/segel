bcc9906fbd12b3fc5069538ef26e480c63f0db31
Mikael Jorhult <mikael@jorhult.se>
2017-08-22 21:48:20 +0200

    Move base test to integration suite.

--- test/integration/contructor.js
+++ test/integration/contructor.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import Segel from '../src/index';
+import Segel from '../../src/index';
 
 test('that constructor is available', function (t) {
   t.is(typeof Segel, 'function');
