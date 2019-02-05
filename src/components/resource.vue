403e9fa4fc3e873cd8191cdf6bca314ae7682bfb
Mikael Jorhult <mikael@jorhult.se>
2019-02-05 10:13:16 +0100

    Move dropzone from directive to component mount.

--- src/components/resource.vue
+++ src/components/resource.vue
@@ -2,7 +2,7 @@
   <li class="segel-resource">
     {{ name }}
 
-    <ul class="segel-bookings" v-dropzone v-on:dblclick="handleDblclick">
+    <ul class="segel-bookings" v-on:dblclick="handleDblclick">
       <segel-booking
         v-for="booking in bookings"
         v-bind:key="booking.id"
@@ -17,8 +17,9 @@
 </template>
 
 <script>
+import interact from "interactjs";
 import SegelBooking from "./booking.vue";
-import Dropzone from "../directives/dropzone.js";
+import Cast from "../helpers/cast";
 import Events from "../helpers/events";
 import Grid from "../helpers/grid";
 
@@ -40,10 +41,6 @@ export default {
     }
   },
 
-  directives: {
-    dropzone: Dropzone
-  },
-
   data: function() {
     return {
       dropTarget: false
@@ -58,7 +55,7 @@ export default {
 
   methods: {
     handleDblclick: function(event) {
-      // Disregard all clicks when Segel is not editable or if event originates another element.
+      // Disregard all clicks when Segel is not editable or if event originates another this.$el.
       if (
         !this.state.config.editable ||
         event.target.className !== "segel-bookings"
@@ -84,6 +81,53 @@ export default {
         end: start + stepSize * 2
       });
     }
+  },
+  mounted: function() {
+    // Wait until all components have been mounted.
+    this.$nextTick(function() {
+      interact(this.$el).dropzone({
+        ondragenter: () => {
+          this.$el.classList.add("droppable");
+        },
+        ondragleave: () => {
+          this.$el.classList.remove("droppable");
+        },
+        ondrop: event => {
+          // Convert moved pixels to change in timestamp.
+          let change = Math.round(
+            (event.dragEvent.dx / this.$root.$el.clientWidth) *
+              this.state.time.duration()
+          );
+
+          // Build object.
+          let booking = {
+            resource: this.id,
+            start: Cast.date(
+              parseInt(event.relatedTarget.__vue__.start) + change
+            ),
+            end: Cast.date(parseInt(event.relatedTarget.__vue__.end) + change)
+          };
+
+          // Create copy if ALT key is pressed, otherwise edit existing.
+          if (event.dragEvent.altKey) {
+            // Emit event to create booking.
+            Events.$emit("bookings-create", booking);
+          } else {
+            // Append ID of moved booking.
+            booking.id = event.relatedTarget.__vue__.id;
+
+            // Emit event to update booking.
+            Events.$emit("bookings-update", booking);
+          }
+
+          this.$el.classList.remove("droppable");
+        }
+      });
+    });
+  },
+  beforeDestroy: function() {
+    // Destroy interact instance on component this.$el.
+    interact(this.$el).unset();
   }
 };
 </script>
